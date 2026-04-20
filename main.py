5d1c219021277363779455d18fc8cd9f7a94c9c3
Zapihra <iida.vaaja@gmail.com>
2026-04-20 13:27:12 +0300

    base of project

---
+++ main.py
@@ -0,0 +1,97 @@
+# output
+# traffic light 1, 2, 3 & 4
+
+# input
+# Walker button 1, 2, 3 & 4
+# Sensor 1, 2, 3 & 4
+
+# errors
+# one faulty sensor (shows there is a car when there is not)
+# one faulty traffic light (lights should show yellow)
+
+#inputs
+def sensor1(detectCar):
+    if detectCar == True:
+        return 20
+
+    return 0
+
+def sensor2(detectCar):
+    if detectCar == True:
+        return 20
+
+    return 0
+
+def sensor3(detectCar):
+    if detectCar == True:
+        return 20
+
+    return 0
+
+def sensor4(detectCar):
+    if detectCar == True:
+        return 20
+
+    return 0
+
+def walkerButton1():
+    buttonPressed = False
+    waitTime = 10
+
+    return
+
+def walkerButton2():
+    buttonPressed = False
+    waitTime = 10
+
+    return
+
+def walkerButton3():
+    buttonPressed = False
+    waitTime = 10
+
+    return
+
+def walkerButton4():
+    buttonPressed = False
+    waitTime = 10
+
+    return
+
+# outputs
+
+def trafficLight1():
+    lightRed = True
+    lightGreen = False
+    lightYellow = False
+
+    return
+
+def trafficLight2():
+    lightRed = True
+    lightGreen = False
+    lightYellow = False
+
+    return
+
+def trafficLight3():
+    lightRed = True
+    lightGreen = False
+    lightYellow = False
+
+    return
+
+def trafficLight4():
+    lightRed = True
+    lightGreen = False
+    lightYellow = False
+
+    return
+
+def main():
+    waitTime = sensor1(True)
+    
+    exit
+
+
+main()
