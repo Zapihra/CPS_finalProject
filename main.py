4899e57ce0ed6eb6325c72629c148da68fb44da1
Zapihra <iida.vaaja@gmail.com>
2026-04-20 14:43:05 +0300

    bases for sensor detection and light change

--- main.py
+++ main.py
@@ -6,92 +6,83 @@
 # Sensor 1, 2, 3 & 4
 
 # errors
-# one faulty sensor (shows there is a car when there is not)
+# one faulty sensor (shows there is a car when there is not, should eventually turn the lights yellow)
 # one faulty traffic light (lights should show yellow)
 
 #inputs
 def sensor1(detectCar):
     if detectCar == True:
         return 20
+    
+    if detectCar == False:
+        return 0
 
     return 0
 
 def sensor2(detectCar):
     if detectCar == True:
         return 20
-
+    
+    if detectCar == False:
+        return 0
     return 0
 
 def sensor3(detectCar):
     if detectCar == True:
         return 20
+    
+    if detectCar == False:
+        return 0
 
     return 0
 
 def sensor4(detectCar):
     if detectCar == True:
         return 20
+    
+    if detectCar == False:
+        return 0
 
     return 0
 
-def walkerButton1():
-    buttonPressed = False
-    waitTime = 10
-
+def walkerButton(walkerButton):
+    if walkerButton == True:
+        return 10
     return
 
-def walkerButton2():
-    buttonPressed = False
-    waitTime = 10
-
-    return
-
-def walkerButton3():
-    buttonPressed = False
-    waitTime = 10
-
-    return
-
-def walkerButton4():
-    buttonPressed = False
-    waitTime = 10
-
-    return
 
 # outputs
 
-def trafficLight1():
-    lightRed = True
-    lightGreen = False
-    lightYellow = False
+def trafficLightChange(lightGreen, lightRed):
+    
+    if lightGreen == True:
+        lightGreen = False
+        lightRed = True
+    else:
+        lightGreen = True
+        lightRed = False
 
-    return
+    return lightGreen, lightRed
 
-def trafficLight2():
-    lightRed = True
-    lightGreen = False
-    lightYellow = False
+def mainDecisionMaker():
+     
+    lightGreen1 = lightGreen2 = lightGreen3 = lightGreen4 = False
+    lightRed1 = lightRed2 = lightRed3 = lightRed4 = True
+    lightYellow1 = lightYellow2 = lightYellow3 = lightYellow4 = False
+    walkerButton1 = walkerButton2 = walkerButton3 = walkerButton4 = False
 
-    return
 
-def trafficLight3():
-    lightRed = True
-    lightGreen = False
-    lightYellow = False
+    table = [True, False, False, True, False, False, True, False]
+    waitTime1 = sensor1(table[0])
+    waitTime2 = sensor2(table[1])
 
-    return
+    print(lightGreen1, lightRed1)
+    
+    lightGreen1, lightRed1 = trafficLightChange(lightGreen1, lightRed1)
+    print(lightGreen1, lightRed1)
 
-def trafficLight4():
-    lightRed = True
-    lightGreen = False
-    lightYellow = False
-
-    return
-
-def main():
-    waitTime = sensor1(True)
     
     exit
 
 
-main()
+mainDecisionMaker()
